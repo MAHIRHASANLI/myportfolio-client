a3a1eab53f6903edf576d5a99b5c515614c1cf8a
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-06-11 20:55:26 +0400

    redeploy

--- src/store/apis/baseURL.js
+++ src/store/apis/baseURL.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://motionless-blue-hippo.cyclic.app";
+export const BASE_URL = "https://myportfolio-server-d8f7.onrender.com"
+// "https://motionless-blue-hippo.cyclic.app";
 // https://hasanli-web-portfolio.onrender.com/api
