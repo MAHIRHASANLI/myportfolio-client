817b29180248379803fc283ee5497db1ea5c43e5
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-10-14 00:54:10 +0530

    yenilendi: SEO optimasiya

--- src/pages/User/MainRoot.jsx
+++ src/pages/User/MainRoot.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import Navbar from "../../component/Navbar";
 import Home from "./Home";
 import About from "./About";
@@ -10,6 +11,19 @@ import BackToTopButton from "../../shared/backToTopButton/BackToTopButton";
 const MainRoot = () => {
   return (
     <React.Fragment>
+       <Helmet>
+        <title>Ana Səhifə - Mahir Hasanli</title>
+        <meta
+          name="description"
+          content="Mahir Hasanli"
+        />
+        <meta name="keywords" content="React js, JavaScript, Portfolio, Mahir Hasanli" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Mahir :: Web Developer" />
+        <meta property="og:description" content="I am a software engineer, developing web applications." />
+        <meta property="og:url" content="https://mahirhasanli.vercel.app" />
+        <meta name="email" content="hasanlimahir1@gmail.com" />
+      </Helmet>
       <Navbar />
       <Home />
       <About />
