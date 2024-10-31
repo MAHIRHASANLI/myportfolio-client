fd7895ab7adb75b6aa8704c7ae2d6fc9fb6b23f5
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-10-31 22:28:38 +0400

    Update myContactApi.js

--- src/store/apis/myContactApi.js
+++ src/store/apis/myContactApi.js
@@ -12,7 +12,7 @@ export const contactApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: BASE_URL,
         fetchFn: async (...args) => {
-            await pouse(1000);
+            await pouse(200);
             return fetch(...args)
         }
     }),
