17e87fb7f48626c9cf1b85f0bee201d7bb7c91c9
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-10-31 22:27:29 +0400

    Update myAboutApi.js

--- src/store/apis/myAboutApi.js
+++ src/store/apis/myAboutApi.js
@@ -12,7 +12,7 @@ export const myAboutApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: BASE_URL,
         fetchFn: async (...args) => {
-            await pouse(1000);
+            await pouse(200);
             return fetch(...args)
         }
     }),
