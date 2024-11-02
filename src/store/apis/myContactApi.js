8e78626ce9e7b96a5c5c6c0003633a0e7e37a533
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-11-02 22:37:53 +0400

    Update myContactApi.js

--- src/store/apis/myContactApi.js
+++ src/store/apis/myContactApi.js
@@ -1,18 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { BASE_URL } from "./baseURL";
 
-const pouse = (duration) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, duration)
-    })
-}
-
 export const contactApi = createApi({
     reducerPath: "contactApi",
     baseQuery: fetchBaseQuery({
         baseUrl: BASE_URL,
-        fetchFn: async (...args) => {
-            await pouse(200);
+        fetchFn: async (...args) => {        
             return fetch(...args)
         }
     }),
